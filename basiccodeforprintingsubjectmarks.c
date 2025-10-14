3a5e7a1c90fd59c8f963690b367f21ce641f58f2
SAAMA-TECH <157895776+SAAMA-TECH@users.noreply.github.com>
2025-10-14 22:39:26 +0530

    Add files via upload

---
+++ basiccodeforprintingsubjectmarks.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/*
+ NAME: SAYYED AFFAN AHMED
+ UIN: 251P088
+ BRANCH: COMPUTER ENGINEERING
+ DIV: D
+*/
+int main() {
+int phy,chem,maths:
+
+float avg:
+
+printf("Enter physics marks:"):
+scanf("%d", &phy):
+
+printf("Enter Chemistry marks:");
+scanf("%d", &chem);
+ 
+printf("Enter Maths marks: ");
+scanf("%d", &maths);
+
+avg (phy + chem maths) / 3;
+printf("Average = %.2f \n",avg);
+(avg >= 50) ? printf("Eligible"): printf("Not Eligible");
+return 0;
+ }
