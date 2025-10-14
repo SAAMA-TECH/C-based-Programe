4f01721289146c0a8b8e90f62489a3c00535244e
SAAMA-TECH <157895776+SAAMA-TECH@users.noreply.github.com>
2025-10-14 22:44:55 +0530

    Add files via upload

---
+++ findifnumberisoddoreven.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ NAME: SAYYED AFFAN AHMED
+ UIN: 251P088
+ BRANCH: COMPUTER ENGINEERING
+ DIV:
+ int main() {
+
+int num1, num2;
+
+printf("Enter the First number: ");
+scanf("%d", &num1);
+
+(num1 % 2 == 0)? printf("%d is Even \n", num1): printf("%d is Odd \n",num1);
+
+printf("Enter the Second number: ");
+scanf("%d", &num2);
+
+(num2 % 2 == 0)? printf("%d is Even", num2): printf("%d is Odd", num2);
+
+return 0;
+}
