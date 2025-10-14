2e19e1a553273986acc994f3897c51d3c25bb4ce
SAAMA-TECH <157895776+SAAMA-TECH@users.noreply.github.com>
2025-10-14 22:43:08 +0530

    Add files via upload

---
+++ basicprintf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/*
+Name of Student: Sayyed Affan Ahmed Maqsood Ahmed
+UIN: 251P088
+Class: Computer Engineering First Year
+Division: D
+Age: 19
+*/
+int main() {
+
+ printf("Name of Student: Sayyed Affan Ahmed Maqsood Ahmed \n"):
+ printf("UIN: 251P088 \n");
+ printf("Class: Computer Engineering First Year \n");
+ printf("Division: D \n");
+ printf("Age: 19 \n");
+ return 0;
+ }
