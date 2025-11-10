32fd0e712939afe67d75d16eeeb8d60196a2f86c
SAAMA-TECH <157895776+SAAMA-TECH@users.noreply.github.com>
2025-11-10 19:36:53 +0530

    Add files via upload

---
+++ findthefactorialofanumberusingiterativeandrecursivefunction.c
@@ -0,0 +1,29 @@
+/* NAME: Sayyed Affan Ahmed
+BRANCH/YEAR: Computer Engineering/F.E
+UIN: 251P088
+ROLL NO: 13
+DIV: D */
+#include <stdio.h>
+// Iterative method
+int factorialIterative(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+    return fact;
+}
+// Recursive method
+int factorialRecursive(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorialRecursive(n - 1);
+}
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("Factorial (Iterative) = %d\n", factorialIterative(num));
+    printf("Factorial (Recursive) = %d\n", factorialRecursive(num));
+    return 0;
+}
