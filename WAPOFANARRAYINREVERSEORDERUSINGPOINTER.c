ecd4a3deb517d12522db4f1145db04382a60f6bf
SAAMA-TECH <157895776+SAAMA-TECH@users.noreply.github.com>
2025-11-28 20:34:03 +0530

    Add files via upload

---
+++ WAPOFANARRAYINREVERSEORDERUSINGPOINTER.c
@@ -0,0 +1,30 @@
+/*
+NAME:SAYYED AFFAN AHMED
+BRANCH/YEAR:COMPUTER_02/F.E
+DIV:D
+UIN/ROLL NO:251P088/13
+*/
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i;
+    int *p;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    p = &a[n - 1];
+
+    printf("Array in reverse order:\n");
+    for (i = n - 1; i >= 0; i--) {
+        printf("%d ", *p);
+        p--;
+    }
+
+    return 0;
+}
